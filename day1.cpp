937675cade4ea8ad83117deaf5220c7bc6853bf1
Harananda <haranandaloukrakpam2@gmail.com>
2026-03-18 19:43:09 +0530

    Add day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>			//this is use to load the prebuild library in cpp, iostream specifically helps in printing to our console
+
+int main(){				//this is call the main function also called the entry point and marks the starting of the code in cpp
+
+	std::cout << "Hello World" <<std:: endl;  //prints hello world		//cout and endl are part of the iostream library
+	std::cout << "I'm learning my cpp from today and I'm going to document it using git" <<std:: endl; 
+
+	return 0;			//this is a way for the main function to send a message to the OS if it finish successfully or is there a problem
+}
