7c41879742937f66bb31c6e8904ea66de1e2371f
Harananda <haranandaloukrakpam2@gmail.com>
2026-03-19 14:25:50 +0530

    Day 2 and 3 commit, I did two lessons in a day

---
+++ day2.cpp
@@ -0,0 +1,13 @@
+//Comments in Cpp
+
+/*This is used to comment out
+	multi lined statement in cpp
+ Comments are used to document your code
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Today's I learn about comments in cpp" << endl;
+return 0;
+}
---
+++ day3.cpp
@@ -0,0 +1,24 @@
+// Errors and Waning in Cpp
+
+/* There are 
+	>Compile time errors
+		The compiler generates an executable binary file from our code which the computer understands.
+		The Compiler has a set of requirements that it enforces on our code for it to compile successfully
+	>Runtime error
+		It's a kind of logical error 
+		The file will compile but it won't run the way we expect it to
+		Sometimes this runtime error can cause the programme to CRASH
+	>Warnings
+		These are the problems that are too minute for the compiler 
+		The codes will compile and run but the compiler still gives the warning that there is a problem
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "This time I learned about the different Errors and Warning in Cpp" << endl;
+return 0;
+}
+
+
