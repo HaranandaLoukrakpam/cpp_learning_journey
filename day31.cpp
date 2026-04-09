edc5e686ffa72b3828bb48bbf357eab8895b249a
Harananda <haranandaloukrakpam2@gmail.com>
2026-04-09 18:40:19 +0530

    break and continue keyword

---
+++ day31.cpp
@@ -0,0 +1,18 @@
+//break and continue keyword
+//break is used to break the loop
+//continue is used to skip current iteration
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	for (int i = 1; i <= 20; i++){
+		if (i ==13){
+			continue; 
+			//break will finish the output at 12
+		}
+	}
+
+
+return 0;
+}
