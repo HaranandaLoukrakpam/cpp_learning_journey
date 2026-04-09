55f017d68746e438e21ecec5969ab5b1b3903498
Harananda <haranandaloukrakpam2@gmail.com>
2026-04-09 18:34:57 +0530

    for loop in C++

---
+++ day30.cpp
@@ -0,0 +1,15 @@
+//for loop in C++
+//used to loop a function for a specified amount of time
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+	for (int i = 1; i <= 13; i ++){
+		cout << i << ". 13 is my roll number" << endl;		//should print this statement 13 times why because i can
+
+
+	}
+return 0;
+}
